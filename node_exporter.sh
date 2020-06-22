ff5da77071e94cd17ca30343751584e7f17e1acb
manoj <manoj4mgl@gmail.com>
2020-06-22 19:47:32 +0800

    Initial commit of node_exporter script

---
+++ node_exporter.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+sudo useradd -M -r -s /bin/false node_exporter
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar xzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo cp node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
+
+sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+sudo cat << EOF >> /etc/systemd/system/node_exporter.service
+     [Unit]
+     Description=node exporter service
+     Wants=network-online.target
+
+     [Service]
+     User=node_exporter
+     Group=node_exporter
+     Type=simple
+     ExecStart=/usr/local/bin/node_exporter
+
+     [Install]
+     WantedBy=multi-user.target
+
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl start node_exporter
